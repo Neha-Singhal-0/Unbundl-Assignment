9c935efe139fb2e59ddcc6eda1efee46656aa563
Neha-Singhal-0 <138149780+Neha-Singhal-0@users.noreply.github.com>
2024-05-03 18:42:03 +0530

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,14 +1,4 @@
-$(document).ready(function () {
-  var autoplaySlider = $("#autoplay").lightSlider({
-    auto: true,
-    loop: true,
-    pauseOnHover: true,
-    onBeforeSlide: function (el) {
-      $("#current").text(el.getCurrentSlideCount());
-    },
-  });
-  $("#total").text(autoplaySlider.getTotalSlideCount());
-});
+
 let bagItems;
 onLoad();
 function onLoad(){
