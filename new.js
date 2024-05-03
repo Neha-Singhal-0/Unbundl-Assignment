3847ee5bd0c29f55af1dcee5e565c83ef332d063
Neha-Singhal-0 <138149780+Neha-Singhal-0@users.noreply.github.com>
2024-05-03 18:41:20 +0530

    added new.js

---
+++ new.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  var autoplaySlider = $("#autoplay").lightSlider({
+    auto: true,
+    loop: true,
+    pauseOnHover: true,
+    onBeforeSlide: function (el) {
+      $("#current").text(el.getCurrentSlideCount());
+    },
+  });
+  $("#total").text(autoplaySlider.getTotalSlideCount());
+});
